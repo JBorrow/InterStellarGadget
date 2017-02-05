0fe8a3d7588542c077b9b3eab981e767549c93ab
Josh Borrow <josh.borrow@gmail.com>
2017-02-05 13:21:00 +0000

    added martizzi eos, need to add constants though

--- code/density.c
+++ code/density.c
@@ -303,9 +303,12 @@ void density(void)
 		SphP[i].DivVel /= SphP[i].Density;
 
 		dt_entr = (All.Ti_Current - (P[i].Ti_begstep + P[i].Ti_endstep) / 2) * All.Timebase_interval;
-
+#ifdef MARTIZZI_EOS
+		SphP[i].Pressure = All.Martizzi_Prefactor * pow(SphP[i].Density, 5./4.);
+#else
 		SphP[i].Pressure =
 		  (SphP[i].Entropy + SphP[i].DtEntropy * dt_entr) * pow(SphP[i].Density, GAMMA);
+#endif
 	      }
 
 
